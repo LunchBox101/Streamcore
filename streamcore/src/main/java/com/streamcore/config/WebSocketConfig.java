abd06a881d0a5051df52f29afd5250faf267f93a
LunchBox101 <lancetleonard@gmail.com>
2026-04-02 11:24:49 -0400

    Phase 2: FFmpeg HLS transcoding via ProcessBuilder

--- streamcore/src/main/java/com/streamcore/config/WebSocketConfig.java
+++ streamcore/src/main/java/com/streamcore/config/WebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 /**
  * WebSocket configuration.
@@ -28,5 +29,13 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(streamWebSocketHandler, "/stream/publish", "/stream/watch")
                 .setAllowedOrigins("*"); // Tighten this in production
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxBinaryMessageBufferSize(10 * 1024 * 1024);
+        container.setMaxTextMessageBufferSize(64 * 1024);
+        return container;
     }
 }
